d185fd84e4e2eb4121f12f71edbff4bf53a3c3ad
Benjamin Lu <b.lu789321@gmail.com>
2024-09-23 23:18:00 -0700

    Third commit: Adjusting backend links for render host

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   // Function to fetch the picks from the API
   const fetchPicks = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/picks', {
+      const response = await fetch('https://blubetbackend.onrender.com', {
         method: 'GET', // Setting the HTTP method
         mode: 'cors',  // Ensuring CORS is handled
         headers: {
